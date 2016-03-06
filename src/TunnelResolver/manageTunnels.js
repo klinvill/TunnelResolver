2ffbe97c79021dd668620569f745faf5e7b9a077
klinvill <kjlinvill@gmail.com>
2016-03-06 16:44:06 -0600

    Reworked the persistent storage to use chrome.storage.local instead of the Event Page since the event page frequently deallocates memory

--- src/TunnelResolver/manageTunnels.js
+++ src/TunnelResolver/manageTunnels.js
@@ -1,11 +1,10 @@
-// Note: the background page stores the tunnels for the extension so that the same settings can be used across multiple tabs
-var DEBUG;
-chrome.runtime.getBackgroundPage(function(background) { DEBUG = background.DEBUG });
+// name of the lookup table in chrome.storage.local
+// state is persisted across sessions, tabs, and popups using chrome.storage.local
+const LOOKUP_TABLE = "lookupTable";
 
 function newTunnel() {
     var source = document.getElementById("src").value;
     var target = document.getElementById("target").value;
-    if (DEBUG) chrome.runtime.getBackgroundPage(function(background) { background.console.log("Source value: "+source+" Target value: "+target) });
     addTunnel(source, target);
 }
 
@@ -21,24 +20,29 @@ function addTunnel(source, target) {
     var sourceParser = new URL(sourceURL);
     var targetParser = new URL(targetURL);
 
-    if (DEBUG) chrome.runtime.getBackgroundPage(function(background) { background.console.log("Parsing source: "+source+" and target: "+target) });
     var sourceHost = sourceParser.host;
     var targetHost = targetParser.host;
 
     // Add source and target mapping to the lookupTable
-    chrome.runtime.getBackgroundPage(function(background) {
-        if (DEBUG) {
-            background.console.log("Generated source host: " + sourceHost);
-            background.console.log("Generated target host: " + targetHost);
-            background.console.log("Adding source to target mapping for: " + sourceHost + " and: " + targetHost);
-        }
-        background.lookupTable.set(sourceHost, targetHost);
+    chrome.storage.local.get(LOOKUP_TABLE, function(results) {
+        var lookupTable= results[LOOKUP_TABLE];
+
+        lookupTable[sourceHost] = targetHost;
+        var value = {};
+        value[LOOKUP_TABLE] = lookupTable;
+        chrome.storage.local.set(value);
     });
 }
 
 function removeTunnel(sourceHost) {
-    // Delete mapping from the lookupTable
-    chrome.runtime.getBackgroundPage(function(background) { background.lookupTable.delete(sourceHost) });
+    chrome.storage.local.get(LOOKUP_TABLE, function(results) {
+        var lookupTable = results[LOOKUP_TABLE];
+        // Delete mapping from the lookupTable
+        delete lookupTable[sourceHost];
+        var storedTable = {};
+        storedTable[LOOKUP_TABLE] = lookupTable;
+        chrome.storage.local.set(storedTable);
+    });
 }
 
 function createTunnelText(source, target) {
@@ -47,22 +51,34 @@ function createTunnelText(source, target) {
 
 document.addEventListener('DOMContentLoaded', function() {
     // List current tunnels
-    chrome.runtime.getBackgroundPage(function(background) {
-        background.lookupTable.forEach(function(target, source, tunnels) {
-            var newLi = document.createElement("li");
-            newLi.appendChild(createTunnelText(source, target));
+    chrome.storage.local.get(LOOKUP_TABLE, function(results) {
+        var lookupTable = results[LOOKUP_TABLE];
+        if (lookupTable) {
+            for(var source in lookupTable) {
 
-            // add a delete button for each listed tunnel
-            var deleteButton = document.createElement("button");
-            deleteButton.appendChild(document.createTextNode("delete"));
-            newLi.appendChild(deleteButton);
-            deleteButton.addEventListener('click', function () {
-                removeTunnel(source);
-                // Since there's very little to reload, it's much easier to just reload the pop-up than to handle selectively removing html elements
-                window.location.reload();
-            });
-        document.getElementById("tunnels").appendChild(newLi);
-        });
+                // check to make sure the key is a property we want to be using
+                if (!lookupTable.hasOwnProperty(source)) continue;
+
+                var target = lookupTable[source];
+                var newLi = document.createElement("li");
+                newLi.appendChild(createTunnelText(source, target));
+
+                // add a delete button for each listed tunnel
+                var deleteButton = document.createElement("button");
+                deleteButton.appendChild(document.createTextNode("delete"));
+                newLi.appendChild(deleteButton);
+
+                // source is a mutable variable used throughout the loop so we need to create a local copy for the event listener callback
+                (function (source) {
+                    deleteButton.addEventListener('click', function () {
+                        removeTunnel(source);
+                        // Since there's very little to reload, it's much easier to just reload the pop-up than to handle selectively removing html elements
+                        window.location.reload();
+                    });
+                }(source));
+                document.getElementById("tunnels").appendChild(newLi);
+            }
+        }
     });
 
     document.getElementById("newTunnel").addEventListener('submit', newTunnel);
