c542e72ca4f39329fb3b6f6d65c0efb063cf6957
klinvill <kjlinvill@gmail.com>
2016-03-06 12:16:13 -0600

    Replaced extension.getBackgroundPage with runtime.getBackgroundPage since this extension uses Event Pages
    I was running into an issue where extension.getBackgroundPage returned null at times. This was due to the fact that event pages are not persisted so you need to use runtime.getBackgroundPage which makes sure that an event page is loaded before calling a callback

--- src/TunnelResolver/manageTunnels.js
+++ src/TunnelResolver/manageTunnels.js
@@ -1,10 +1,11 @@
 // Note: the background page stores the tunnels for the extension so that the same settings can be used across multiple tabs
-var DEBUG = chrome.extension.getBackgroundPage().DEBUG;
+var DEBUG;
+chrome.runtime.getBackgroundPage(function(background) { DEBUG = background.DEBUG });
 
 function newTunnel() {
     var source = document.getElementById("src").value;
     var target = document.getElementById("target").value;
-    if (DEBUG) chrome.extension.getBackgroundPage().console.log("Source value: "+source+" Target value: "+target);
+    if (DEBUG) chrome.runtime.getBackgroundPage(function(background) { background.console.log("Source value: "+source+" Target value: "+target) });
     addTunnel(source, target);
 }
 
@@ -14,29 +15,30 @@ function addTunnel(source, target) {
     var targetURL;
 
     // appends http:// if needed to get a valid url
-    if (!(source.substring(0,7)=="http://" || source.substring(0,8)=="https://")) sourceURL = "http://".concat(source)
-    if (!(target.substring(0,7)=="http://" || target.substring(0,8)=="https://")) targetURL = "http://".concat(target)
+    if (!(source.substring(0,7)=="http://" || source.substring(0,8)=="https://")) sourceURL = "http://".concat(source);
+    if (!(target.substring(0,7)=="http://" || target.substring(0,8)=="https://")) targetURL = "http://".concat(target);
 
     var sourceParser = new URL(sourceURL);
     var targetParser = new URL(targetURL);
 
-    if (DEBUG) chrome.extension.getBackgroundPage().console.log("Parsing source: "+source+" and target: "+target);
+    if (DEBUG) chrome.runtime.getBackgroundPage(function(background) { background.console.log("Parsing source: "+source+" and target: "+target) });
     var sourceHost = sourceParser.host;
     var targetHost = targetParser.host;
 
-    if (DEBUG) chrome.extension.getBackgroundPage().console.log("Generated source host: "+sourceHost);
-    if (DEBUG) chrome.extension.getBackgroundPage().console.log("Generated target host: "+targetHost);
-    if (DEBUG) chrome.extension.getBackgroundPage().console.log("Adding source to target mapping for: "+sourceHost+" and: "+targetHost);
-
     // Add source and target mapping to the lookupTable
-    chrome.extension.getBackgroundPage().lookupTable.set(sourceHost, targetHost);
+    chrome.runtime.getBackgroundPage(function(background) {
+        if (DEBUG) {
+            background.console.log("Generated source host: " + sourceHost);
+            background.console.log("Generated target host: " + targetHost);
+            background.console.log("Adding source to target mapping for: " + sourceHost + " and: " + targetHost);
+        }
+        background.lookupTable.set(sourceHost, targetHost);
+    });
 }
 
 function removeTunnel(sourceHost) {
     // Delete mapping from the lookupTable
-    chrome.extension.getBackgroundPage().lookupTable.delete(sourceHost);
-    // Since there's very little to reload, it's much easier to just reload the pop-up than to handle selectively removing html elements
-    window.location.reload();
+    chrome.runtime.getBackgroundPage(function(background) { background.lookupTable.delete(sourceHost) });
 }
 
 function createTunnelText(source, target) {
@@ -45,15 +47,22 @@ function createTunnelText(source, target) {
 
 document.addEventListener('DOMContentLoaded', function() {
     // List current tunnels
-    chrome.extension.getBackgroundPage().lookupTable.forEach(function(target, source, tunnels) {
-        var newLi = document.createElement("li");
-        newLi.appendChild(createTunnelText(source, target));
-        var deleteButton = document.createElement("button").appendChild(document.createTextNode("delete"));
-        newLi.appendChild(deleteButton);
-        deleteButton.addEventListener('click', function() {removeTunnel(source)});
+    chrome.runtime.getBackgroundPage(function(background) {
+        background.lookupTable.forEach(function(target, source, tunnels) {
+            var newLi = document.createElement("li");
+            newLi.appendChild(createTunnelText(source, target));
+
+            // add a delete button for each listed tunnel
+            var deleteButton = document.createElement("button").appendChild(document.createTextNode("delete"));
+            newLi.appendChild(deleteButton);
+            deleteButton.addEventListener('click', function () {
+                removeTunnel(source);
+                // Since there's very little to reload, it's much easier to just reload the pop-up than to handle selectively removing html elements
+                window.location.reload();
+            });
         document.getElementById("tunnels").appendChild(newLi);
+        });
     });
 
     document.getElementById("newTunnel").addEventListener('submit', newTunnel);
-    if (DEBUG) chrome.extension.getBackgroundPage().console.log("Loaded!!!");
 });
